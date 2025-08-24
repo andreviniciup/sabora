073f9cedb529891fc0d7e01017781b8e30b7e7cb
vinicius <andreviniciusrp@gmail.com>
2025-08-24 17:03:02 -0300

    feat: atualizar dicionários de sinônimos para melhorar reconhecimento de linguagem natural

--- backend/src/nlp/synonyms.py
+++ backend/src/nlp/synonyms.py
@@ -1,36 +1,103 @@
 CULINARIA = {
-    "nordestina": ["nordestina", "regional", "carne de sol", "cuscuz", "acarajé", "baião de dois"],
-    "italiana": ["italiana", "pizza", "massa", "macarrão", "macarronada", "lasanha"],
-    "japonesa": ["japonesa", "japa", "sushi", "temaki", "japonês", "sashimi", "uramaki"],
-    "brasileira": ["brasileira", "feijão", "feijoada", "quentinha"],
-    "chinesa": ["chinesa", "china", "chop suey", "yakisoba", "rolinho primavera", "bao"],
-    "árabe": ["árabe", "kibe", "esfiha", "shawarma", "tabule"],
-    "portuguesa": ["portuguesa", "bacalhau", "pastel de nata", "sardinha"],
-    "peruana": ["peruana", "ceviche", "pisco", "lomo saltado", "chicharrón", "chaufa"],
-    "mediterrânea": ["mediterrânea", "hummus", "falafel", "tabule", "pita"],
-    "mexicana": ["mexicana", "taco", "burrito", "guacamole", "quesadilla"],
-    "indiana": ["indiana", "curry", "tandoori", "masala", "naan"],
-    "francesa": ["francesa", "croissant", "crepe", "ratatouille"],
-    "frutos do mar": ["frutos do mar", "peixe", "camarão", "mariscos", "polvo", "lagosta", "siri"],
-    "vegana": ["vegana", "vegano", "vegetariana", "vegetariano", "plant based"],
-    "saldável": ["saudável", "fit", "light", "low carb", "dieta", "salada"],
-    "fast food": ["fast food", "lanche", "hamburguer", "pizza", "batata frita", "hot dog", "sanduíche", "passaporte"],
-    "padaria": ["padaria", "pão", "bolo", "doces", "salgados", "bolacha"],
+    "nordestina": [
+        "nordestina", "regional", "carne de sol", "cuscuz", "acaraje", "baiao de dois",
+        "sarapatel", "tapioca", "bode", "buchada", "dobradinha", "moqueca", "peixada"
+    ],
+    "italiana": [
+        "italiana", "pizza", "massa", "macarrao", "macarronada", "lasanha", "risoto",
+        "gnocchi", "nhoque", "espaguete", "fettuccine", "carbonara", "bolonhesa"
+    ],
+    "japonesa": [
+        "japonesa", "japa", "sushi", "suchi", "temaki", "sashimi", "uramaki", "yakimeshi",
+        "robata", "gyoza", "missoshiro", "ramen", "lamen"
+    ],
+    "brasileira": [
+        "brasileira", "feijao", "feijoada", "quentinha", "prato feito", "pf", "churrasco",
+        "farofa", "arroz com feijao", "virado", "moqueca baiana"
+    ],
+    "chinesa": [
+        "chinesa", "china", "chop suey", "yakisoba", "rolinho primavera", "bao", "dumpling",
+        "frango xadrez", "porco agridoce"
+    ],
+    "arabe": [
+        "arabe", "kibe", "quibe", "esfiha", "shawarma", "tabule", "homus", "coalhada", "pita",
+        "kafta"
+    ],
+    "portuguesa": [
+        "portuguesa", "bacalhau", "pastel de nata", "sardinha", "caldo verde", "arroz de pato"
+    ],
+    "peruana": [
+        "peruana", "ceviche", "pisco", "lomo saltado", "chicharron", "chaufa", "aji de gallina"
+    ],
+    "mediterranea": [
+        "mediterranea", "hummus", "homus", "falafel", "tabule", "pita", "azeitona", "azeite",
+        "cuscuz marroquino", "ratatouille"
+    ],
+    "mexicana": [
+        "mexicana", "taco", "burrito", "guacamole", "quesadilla", "nachos", "enchilada",
+        "chili", "fajita"
+    ],
+    "indiana": [
+        "indiana", "curry", "tandoori", "masala", "naan", "chutney", "dal", "samosa",
+        "biryani"
+    ],
+    "francesa": [
+        "francesa", "croissant", "crepe", "ratatouille", "quiche", "souffle", "baguete"
+    ],
+    "frutos do mar": [
+        "frutos do mar", "peixe", "camarao", "mariscos", "polvo", "lagosta", "siri",
+        "ostra", "mexilhao", "lula"
+    ],
+    "vegana": [
+        "vegana", "vegano", "vegetariana", "vegetariano", "plant based", "sem carne",
+        "sem derivados animais", "sem lactose", "sem ovo", "vegetal"
+    ],
+    "saudavel": [
+        "saudavel", "fit", "light", "low carb", "dieta", "salada", "natural", "integral",
+        "organico", "funcional", "wellness", "detox"
+    ],
+    "fast food": [
+        "fast food", "lanche", "hamburguer", "hamburgueria", "pizza", "batata frita",
+        "hot dog", "cachorro quente", "sanduiche", "passaporte", "milkshake", "mc donalds",
+        "burguer king", "bk", "mcdonalds", "subway"
+    ],
+    "padaria": [
+        "padaria", "pao", "bolo", "doces", "salgados", "bolacha", "biscoito", "cafe da manha",
+        "croissant", "empada", "torta"
+    ],
 }
 
-
-
 PRECO = {
-    "baixo": ["barato", "baratos", "economico", "econômico", "econômicos", "acessivel", "acessível", "acessíveis", "em conta", "popular", "modesto", "baratinho", "custo-benefício", "promocional", "promoçao", "promoção", "preço baixo", "preços baixos"],
-    "alto": ["caro", "caros", "luxo", "luxuoso", "luxuosos", "premium", "elevado", "sofisticado", "exclusivo", "alto custo", "gourmet"],
+    "baixo": [
+        "barato", "baratos", "economico", "economicos", "acessivel", "acessiveis",
+        "em conta", "popular", "modesto", "baratinho", "custo beneficio", "promocional",
+        "promocao", "preco baixo", "precos baixos", "pechincha", "de oferta"
+    ],
+    "alto": [
+        "caro", "caros", "luxo", "luxuoso", "luxuosos", "premium", "elevado",
+        "sofisticado", "exclusivo", "alto custo", "gourmet", "fino", "carissimo"
+    ],
 }
 
 DISTANCIA = {
-    "perto": ["perto", "próximo", "proximo", "perto de mim", "proximo de mim", "aqui perto", "vizinho", "nas redondezas", "ao lado", "região próxima", "mais perto"],
-    "longe": ["longe", "distante", "afastado", "mais afastado", "distante daqui"],
+    "perto": [
+        "perto", "proximo", "perto de mim", "proximo de mim", "aqui perto",
+        "vizinho", "nas redondezas", "ao lado", "regiao proxima", "mais perto",
+        "na esquina", "do lado"
+    ],
+    "longe": [
+        "longe", "distante", "afastado", "mais afastado", "distante daqui",
+        "outra cidade", "periferia", "muito longe"
+    ],
 }
 
 AVALIACAO = {
-    "bom": ["bom", "otimo", "excelente", "top", "maravilhoso", "incrível", "perfeito", "nota 10"],
-    "ruim":["ruim", "péssimo", "horrível", "fraco", "insatisfatório", "decepcionante", "regular"],
+    "bom": [
+        "bom", "otimo", "excelente", "top", "topzera", "maravilhoso", "incrivel",
+        "perfeito", "nota 10", "show", "da hora", "massa", "brabo"
+    ],
+    "ruim": [
+        "ruim", "pessimo", "horrivel", "fraco", "insatisfatorio", "decepcionante",
+        "regular", "meh", "mais ou menos", "fraquinho", "sofrivel", "ruinzinho"
+    ],
 }
