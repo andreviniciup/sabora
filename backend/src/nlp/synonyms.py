db2b8410d408053842db82f77c3fe6788334299e
vinicius <andreviniciusrp@gmail.com>
2025-08-24 23:44:31 -0300

    feat: expandir sinônimos e padrões de validação de busca

--- backend/src/nlp/synonyms.py
+++ backend/src/nlp/synonyms.py
@@ -1,103 +1,225 @@
 CULINARIA = {
     "nordestina": [
-        "nordestina", "regional", "carne de sol", "cuscuz", "acaraje", "baiao de dois",
-        "sarapatel", "tapioca", "bode", "buchada", "dobradinha", "moqueca", "peixada"
+        "nordestina", "nordestino", "regional", "carne de sol", "cuscuz", "acaraje", "baiao de dois",
+        "sarapatel", "tapioca", "bode", "buchada", "dobradinha", "moqueca", "peixada",
+        "comida nordestina", "culinaria nordestina", "pratos nordestinos", "tempero nordestino",
+        "dendê", "queijo coalho", "macaxeira", "farofa de mandioca", "pirão", "bobó",
+        "vatapá", "caruru", "xinxim", "mocotó", "panelada", "galinha de cabidela"
     ],
     "italiana": [
-        "italiana", "pizza", "massa", "macarrao", "macarronada", "lasanha", "risoto",
-        "gnocchi", "nhoque", "espaguete", "fettuccine", "carbonara", "bolonhesa"
+        "italiana", "italiano", "pizza", "pizzaria", "massa", "macarrao", "macarronada", 
+        "lasanha", "risoto", "gnocchi", "nhoque", "espaguete", "fettuccine", "carbonara", 
+        "bolonhesa", "parmegiana", "margherita", "quattro stagioni", "calzone", "bruschetta",
+        "antipasto", "minestrone", "ossobuco", "tiramisù", "gelato", "panna cotta",
+        "pasta", "ravioli", "tortellini", "cannelloni", "linguine", "penne", "fusilli"
     ],
     "japonesa": [
-        "japonesa", "japonês", "japones", "japa", "sushi", "suchi", "temaki", "sashimi", "uramaki", "yakimeshi",
-        "robata", "gyoza", "missoshiro", "ramen", "lamen"
+        "japonesa", "japonês", "japones", "japa", "sushi", "suchi", "temaki", "sashimi", 
+        "uramaki", "yakimeshi", "robata", "gyoza", "missoshiro", "ramen", "lamen",
+        "yakissoba", "makizushi", "niguiri", "hot roll", "california roll", "philadelphia roll",
+        "sunomono", "edamame", "teriyaki", "tenpura", "tempura", "yakitori", "donburi",
+        "chirashi", "miso", "shoyu", "wasabi", "gengibre", "nori", "sake", "rodizio japa"
     ],
     "brasileira": [
-        "brasileira", "feijao", "feijoada", "quentinha", "prato feito", "pf", "churrasco",
-        "farofa", "arroz com feijao", "virado", "moqueca baiana"
+        "brasileira", "brasileiro", "feijao", "feijoada", "quentinha", "prato feito", "pf", 
+        "churrasco", "churrascaria", "farofa", "arroz com feijao", "virado", "moqueca baiana",
+        "picanha", "costela", "fraldinha", "maminha", "alcatra", "cupim", "linguiça",
+        "pão de açúcar", "brigadeiro", "açaí", "coxinha", "pastel", "sanduiche natural",
+        "tropeiro", "tutu de feijão", "couve refogada", "mandioca", "polenta", "pamonha"
     ],
     "chinesa": [
-        "chinesa", "china", "chop suey", "yakisoba", "rolinho primavera", "bao", "dumpling",
-        "frango xadrez", "porco agridoce"
+        "chinesa", "china", "chines", "chop suey", "yakisoba", "rolinho primavera", "bao", 
+        "dumpling", "frango xadrez", "porco agridoce", "sweet and sour", "kung pao",
+        "mapo tofu", "hot pot", "dim sum", "wonton", "chow mein", "fried rice",
+        "pato laqueado", "frango empanado", "camarão empanado", "agridoce", "shoyu"
     ],
     "arabe": [
-        "arabe", "kibe", "quibe", "esfiha", "shawarma", "tabule", "homus", "coalhada", "pita",
-        "kafta"
+        "arabe", "árabe", "kibe", "quibe", "esfiha", "esfirra", "shawarma", "tabule", 
+        "homus", "hummus", "coalhada", "pita", "kafta", "baklava", "halloumi",
+        "falafel", "labneh", "fattoush", "muhammara", "za'atar", "tahine", "tahini",
+        "kebab", "churrasco árabe", "kibbeh", "sambusek", "manakish"
     ],
     "portuguesa": [
-        "portuguesa", "bacalhau", "pastel de nata", "sardinha", "caldo verde", "arroz de pato"
+        "portuguesa", "portugues", "bacalhau", "pastel de nata", "sardinha", "caldo verde", 
+        "arroz de pato", "francesinha", "bifana", "cozido à portuguesa", "linguiça",
+        "alheira", "chouriço", "pastéis de bacalhau", "feijoada à transmontana",
+        "arroz de marisco", "cataplana", "açorda", "migas"
     ],
     "peruana": [
-        "peruana", "ceviche", "pisco", "lomo saltado", "chicharron", "chaufa", "aji de gallina"
+        "peruana", "peruano", "ceviche", "pisco", "lomo saltado", "chicharron", "chaufa", 
+        "aji de gallina", "papa rellena", "anticuchos", "causa limeña", "tiradito",
+        "rocoto relleno", "seco de cabrito", "tacu tacu", "picarones", "suspiro limeño"
     ],
     "mediterranea": [
-        "mediterranea", "hummus", "homus", "falafel", "tabule", "pita", "azeitona", "azeite",
-        "cuscuz marroquino", "ratatouille"
+        "mediterranea", "mediterrâneo", "hummus", "homus", "falafel", "tabule", "pita", 
+        "azeitona", "azeite", "cuscuz marroquino", "ratatouille", "moussaka", "tzatziki",
+        "dolmas", "spanakopita", "souvlaki", "gyros", "baklava", "halva"
     ],
     "mexicana": [
-        "mexicana", "taco", "burrito", "guacamole", "quesadilla", "nachos", "enchilada",
-        "chili", "fajita"
+        "mexicana", "mexicano", "taco", "burrito", "guacamole", "quesadilla", "nachos", 
+        "enchilada", "chili", "fajita", "tortilla", "salsa", "pico de gallo", "mole",
+        "tamale", "elote", "pozole", "ceviche mexicano", "margarita", "tequila"
     ],
     "indiana": [
-        "indiana", "curry", "tandoori", "masala", "naan", "chutney", "dal", "samosa",
-        "biryani"
+        "indiana", "indiano", "curry", "tandoori", "masala", "naan", "chutney", "dal", 
+        "samosa", "biryani", "tikka masala", "vindaloo", "korma", "roti", "chapati",
+        "lassi", "mango lassi", "chai", "paneer", "basmati", "ghee", "garam masala"
     ],
     "francesa": [
-        "francesa", "croissant", "crepe", "ratatouille", "quiche", "souffle", "baguete"
+        "francesa", "francês", "frances", "croissant", "crepe", "ratatouille", "quiche", 
+        "souffle", "baguete", "baguette", "bouillabaisse", "coq au vin", "escargot",
+        "foie gras", "confit", "cassoulet", "croque monsieur", "pain au chocolat",
+        "macaron", "eclair", "mille-feuille", "tarte tatin", "crème brûlée"
     ],
     "frutos do mar": [
-        "frutos do mar", "peixe", "camarao", "mariscos", "polvo", "lagosta", "siri",
-        "ostra", "mexilhao", "lula"
+        "frutos do mar", "peixe", "camarao", "camarão", "mariscos", "polvo", "lagosta", 
+        "siri", "ostra", "mexilhao", "mexilhão", "lula", "pescado", "marisco",
+        "caranguejo", "bacalhau", "salmão", "robalo", "dourado", "linguado", "badejo",
+        "moqueca de peixe", "casquinha de siri", "bobo de camarão", "paella"
     ],
     "vegana": [
         "vegana", "vegano", "vegetariana", "vegetariano", "plant based", "sem carne",
-        "sem derivados animais", "sem lactose", "sem ovo", "vegetal"
+        "sem derivados animais", "sem lactose", "sem ovo", "vegetal", "plantas",
+        "verde", "natural", "cruelty free", "100% vegetal", "à base de plantas",
+        "hambúrguer vegano", "leite vegetal", "queijo vegano", "carne vegetal"
     ],
     "saudavel": [
-        "saudavel", "fit", "light", "low carb", "dieta", "salada", "natural", "integral",
-        "organico", "funcional", "wellness", "detox"
+        "saudavel", "saudável", "fit", "fitness", "light", "low carb", "dieta", "salada", 
+        "natural", "integral", "organico", "orgânico", "funcional", "wellness", "detox",
+        "proteína", "whey", "smoothie", "açaí", "granola", "quinoa", "chia", "aveia",
+        "sem glúten", "sem açúcar", "zero açúcar", "diet", "nutritivo", "balanceado"
     ],
     "fast food": [
-        "fast food", "lanche", "hamburguer", "hamburgueria", "pizza", "batata frita",
-        "hot dog", "cachorro quente", "sanduiche", "passaporte", "milkshake", "mc donalds",
-        "burguer king", "bk", "mcdonalds", "subway"
+        "fast food", "lanche", "hamburguer", "hambúrguer", "hamburgueria", "pizza", 
+        "batata frita", "hot dog", "cachorro quente", "sanduiche", "sanduba", "x-burger",
+        "milkshake", "mc donalds", "mcdonald's", "mcdonalds", "burguer king", "bk", 
+        "burger king", "subway", "kfc", "pizza hut", "dominos", "ifood", "delivery",
+        "drive thru", "combo", "big mac", "whopper", "quarterão", "taco bell",
+        "giraffas", "bobs", "habib's", "habibs", "spoleto", "china in box"
     ],
     "padaria": [
-        "padaria", "pao", "bolo", "doces", "salgados", "bolacha", "biscoito", "cafe da manha",
-        "croissant", "empada", "torta"
+        "padaria", "pao", "pão", "bolo", "doces", "salgados", "bolacha", "biscoito", 
+        "cafe da manha", "café da manhã", "croissant", "empada", "torta", "sonho",
+        "rosquinha", "pão de açúcar", "pão francês", "pão integral", "baguete",
+        "folhado", "pastel", "coxinha", "enroladinho", "quindim", "bem-casado",
+        "bomba", "carolinas", "éclair", "mil folhas", "confeitaria", "doceria"
     ],
+    "cafe": [
+        "café", "cafe", "cafeteria", "coffee shop", "espresso", "cappuccino", "latte",
+        "macchiato", "mocha", "americano", "café gelado", "frappé", "affogato",
+        "starbucks", "coffee break", "barista", "grão", "torra", "blend",
+        "café da manhã", "brunch", "açaí", "smoothie", "chá", "tea"
+    ],
+    "bar": [
+        "bar", "boteco", "botequim", "pub", "cervejaria", "choperia", "drinks",
+        "petiscos", "happy hour", "caipirinha", "cerveja", "chopp", "whisky",
+        "vodka", "gin", "rum", "cachaça", "vinho", "sangria", "mojito",
+        "aperitivo", "tira-gosto", "porção", "amendoim", "azeitona", "queijo"
+    ]
+}
+
+# Adicionar estabelecimentos famosos
+ESTABELECIMENTOS_FAMOSOS = {
+    "mcdonalds": ["mcdonalds", "mcdonald's", "mc donalds", "mc donald's", "mequi", "arcos dourados"],
+    "burger_king": ["burger king", "bk", "burguer king", "rei do hambúrguer"],
+    "subway": ["subway", "sub way"],
+    "kfc": ["kfc", "kentucky", "kentucky fried chicken"],
+    "pizza_hut": ["pizza hut", "pizzahut"],
+    "dominos": ["domino's", "dominos pizza", "dominos"],
+    "starbucks": ["starbucks", "star bucks"],
+    "bobs": ["bob's", "bobs"],
+    "giraffas": ["giraffas", "girafas"],
+    "habibs": ["habib's", "habibs", "habib"],
+    "spoleto": ["spoleto"],
+    "china_in_box": ["china in box", "china inbox", "chinainbox"],
+    "outback": ["outback", "outback steakhouse"],
+    "tgi_fridays": ["tgi friday's", "tgi fridays", "fridays"],
+    "ragazzo": ["ragazzo"],
+    "pizza_ranch": ["pizza ranch"],
+    "si_senor": ["si señor", "si senor"],
+    "america": ["américa", "america"],
+    "applebees": ["applebee's", "applebees"],
+    "madero": ["madero", "hamburgueria do futuro"],
+    "the_fifties": ["the fifties", "fifties"],
+    "degree": ["degree"],
+    "z_deli": ["z deli", "zdeli"],
+    "jeronimo": ["jerônimo", "jeronimo"],
+    "le_pain_quotidien": ["le pain quotidien"],
+    "bread_bakery": ["bread & bakery", "bread bakery"],
+    "santa_felicidade": ["santa felicidade"],
+    "cantina_do_lucas": ["cantina do lucas"],
+    "bella_paulista": ["bella paulista"],
+    "casa_do_bacalhau": ["casa do bacalhau"],
+    "churrascaria_palace": ["churrascaria palace", "palace"],
+    "fogo_de_chao": ["fogo de chão", "fogo de chao"],
+    "espetus": ["espetus churrascaria", "espetus"],
+    "galeto_sat": ["galeto sat", "galeto sats"],
+    "vieira_souto": ["vieira souto"],
+    "capim_santo": ["capim santo"],
+    "mamma_jamma": ["mamma jamma", "mama jama"],
+    "red_angus": ["red angus"],
+    "costelao": ["costelão", "costelao"],
+    "picanha_na_brasa": ["picanha na brasa"],
+    "varanda": ["varanda"],
+    "Giuseppe": ["giuseppe"],
+    "Famiglia_Mancini": ["famiglia mancini", "mancini"],
+    "Sujinho": ["sujinho"],
+    "Pamonharia": ["pamonharia"],
+    "Rei_do_Mate": ["rei do mate"]
 }
 
 PRECO = {
     "baixo": [
-        "barato", "baratos", "economico", "economicos", "acessivel", "acessiveis",
-        "em conta", "popular", "modesto", "baratinho", "custo beneficio", "promocional",
-        "promocao", "preco baixo", "precos baixos", "pechincha", "de oferta"
+        "barato", "baratos", "economico", "economicos", "económico", "económicos",
+        "acessivel", "acessiveis", "acessível", "acessíveis", "em conta", "popular", 
+        "modesto", "baratinho", "custo beneficio", "custo benefício", "promocional",
+        "promocao", "promoção", "preco baixo", "preço baixo", "precos baixos", 
+        "preços baixos", "pechincha", "de oferta", "conta pequena", "preço justo",
+        "vale a pena", "bom preço", "preço amigo", "preço camarada", "sem gastar muito",
+        "orçamento apertado", "estudante", "universitário", "popular", "do povo"
     ],
     "alto": [
         "caro", "caros", "luxo", "luxuoso", "luxuosos", "premium", "elevado",
-        "sofisticado", "exclusivo", "alto custo", "gourmet", "fino", "carissimo"
+        "sofisticado", "exclusivo", "alto custo", "gourmet", "fino", "carissimo",
+        "caríssimo", "chique", "elegante", "requintado", "classe a", "primeira linha",
+        "alto padrão", "top de linha", "seleto", "diferenciado", "especial",
+        "gastronomia", "gastronômico", "chef renomado", "cinco estrelas"
     ],
 }
 
 DISTANCIA = {
     "perto": [
-        "perto", "proximo", "perto de mim", "proximo de mim", "aqui perto",
-        "vizinho", "nas redondezas", "ao lado", "regiao proxima", "mais perto",
-        "na esquina", "do lado"
+        "perto", "proximo", "próximo", "perto de mim", "proximo de mim", "próximo de mim",
+        "aqui perto", "vizinho", "nas redondezas", "ao lado", "regiao proxima", 
+        "região próxima", "mais perto", "na esquina", "do lado", "vizinhança",
+        "bairro", "na área", "na região", "localizado perto", "fica perto",
+        "caminhada", "a pé", "curtinha distância", "ali perto", "bem perto"
     ],
     "longe": [
         "longe", "distante", "afastado", "mais afastado", "distante daqui",
-        "outra cidade", "periferia", "muito longe"
+        "outra cidade", "periferia", "muito longe", "zona", "interior",
+        "subúrbio", "afastado do centro"
     ],
 }
 
 AVALIACAO = {
     "bom": [
-        "bom", "otimo", "excelente", "top", "topzera", "maravilhoso", "incrivel",
-        "perfeito", "nota 10", "show", "da hora", "massa", "brabo"
+        "bom", "boa", "bons", "boas", "otimo", "ótimo", "ótimos", "otimos", 
+        "excelente", "excelentes", "top", "topzera", "maravilhoso", "maravilhosa",
+        "incrivel", "incrível", "perfeito", "perfeita", "nota 10", "show", 
+        "da hora", "massa", "brabo", "brabíssimo", "sensacional", "fantástico",
+        "espetacular", "fenomenal", "extraordinário", "excepcional", "premium",
+        "cinco estrelas", "4 estrelas", "bem avaliado", "recomendado", "indicado",
+        "aprovado", "testado", "confiável", "de qualidade", "referência"
     ],
     "ruim": [
-        "ruim", "pessimo", "horrivel", "fraco", "insatisfatorio", "decepcionante",
-        "regular", "meh", "mais ou menos", "fraquinho", "sofrivel", "ruinzinho"
+        "ruim", "ruins", "pessimo", "péssimo", "péssimos", "pessimos", 
+        "horrivel", "horrível", "horríveis", "fraco", "fracos", "fraca", 
+        "insatisfatorio", "insatisfatório", "decepcionante", "regular", 
+        "meh", "mais ou menos", "fraquinho", "sofrivel", "sofrível", 
+        "ruinzinho", "não recomendo", "evitar", "não vale a pena"
     ],
 }
+
+# Combinar estabelecimentos famosos com fast food
+CULINARIA["fast food"].extend([name for names in ESTABELECIMENTOS_FAMOSOS.values() for name in names])
